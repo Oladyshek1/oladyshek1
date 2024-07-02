960c604c4da55ecebb0832711e995ada4a4d02e8
Vika <fool.vils@Gmail.com>
2024-07-02 09:46:52 +0500

    Великий ФИНАЛ!

--- src/main/java/com/example/practika/HelloApplication.java
+++ src/main/java/com/example/practika/HelloApplication.java
@@ -2,19 +2,8 @@ package com.example.practika;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-
-
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
@@ -24,7 +13,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 700, 450);
-        stage.setTitle("Hello!");
+        stage.setTitle("Ежедневник");
         stage.setScene(scene);
         stage.show();
     }
